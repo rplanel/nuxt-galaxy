ba6f554c8175580110d77289fe42f9b53fa6a900
Remi  PLANEL <rplanel@pasteur.fr>
2024-12-10 15:35:55 +0100

    Fix: set supabase env var to fix tests

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -4,4 +4,12 @@ export default defineNuxtConfig({
   modules: [
     MyModule,
   ],
+  runtimeConfig: {
+    public: {
+      supabase: { url: 'http://localhost:54323', key: 'anno-key' },
+    },
+
+  },
+  compatibilityDate: '2024-12-07',
+
 })
