94b010b574f9b87fd678e46bf0e702b6f8837f3b
Remi  PLANEL <rplanel@pasteur.fr>
2024-12-10 22:23:28 +0100

    fix: error TS2742: The inferred type of default for middleware auth cf: https://github.com/nuxt/module-builder/issues/141

--- src/runtime/app/middleware/auth.ts
+++ src/runtime/app/middleware/auth.ts
@@ -1,6 +1,7 @@
+import type {} from 'nuxt/app'
 import { defineNuxtRouteMiddleware, navigateTo, useSupabaseUser } from '#imports'
 
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware((to) => {
   const user = useSupabaseUser()
   if (!user.value) return navigateTo(`/login?redirectTo=${to.path}`)
 })
