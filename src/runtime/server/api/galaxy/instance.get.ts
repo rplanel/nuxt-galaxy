93ca01b0cb3f071a22de005cd439ae1eaadf9c54
Remi  PLANEL <rplanel@pasteur.fr>
2024-12-16 17:46:33 +0100

    fix(type): add explicit type

--- src/runtime/server/api/galaxy/instance.get.ts
+++ src/runtime/server/api/galaxy/instance.get.ts
@@ -1,11 +1,11 @@
 // import { useRuntimeConfig } from '@nuxt/kit'
-import type { GalaxyClient } from '@rplanel/galaxy-js'
+import type { GalaxyClient, GalaxyVersion } from '@rplanel/galaxy-js'
 import { defineEventHandler } from 'h3'
 import { useRuntimeConfig } from '#imports'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (event): Promise<{ url: string, version_major: string, version_minor: string }> => {
   const { public: { galaxy: { url } } } = useRuntimeConfig()
   const $galaxy: GalaxyClient = event.context?.galaxy
-  const version = await $galaxy.getVersion()
+  const version: GalaxyVersion = await $galaxy.getVersion()
   return { url, ...version }
 })
